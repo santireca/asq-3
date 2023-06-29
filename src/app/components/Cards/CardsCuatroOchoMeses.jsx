fff93079ff28802d7966e197bdb0adb6b6962bb9
santireca <santiagorecabarren7@gmail.com>
2023-06-29 00:00:39 -0300

    6th commit

--- src/app/components/Cards/CardsCuatroOchoMeses.jsx
+++ src/app/components/Cards/CardsCuatroOchoMeses.jsx
@@ -14,20 +14,20 @@ export default function CardsCuatroOchoMeses (){
                 >Entre 4 y 8 meses de edad </button>
             {
                 mostrarCards && (
-                    <div className=" flex flex-wrap justify-center gap-10 mb-10 mt-10">
+                    <div className=" flex flex-wrap justify-center gap-10 mb-10 mt-10 sm:gap-5">
                         
                 {cuatroOchoMeses.map((actividad) => (
-                    <div key={actividad.id} className=" w-[500px] h-[200px] flex flex-col justify-between bg-white bg-opacity-70 shadow-lg backdrop-filter backdrop-blur-md border border-opacity-18 p-5 rounded-md cursor-pointer hover:scale-105 transition duration-300 ">
+                    <div key={actividad.id} className=" w-[500px] h-[200px] flex flex-col justify-between bg-white bg-opacity-70 shadow-lg backdrop-filter backdrop-blur-md border border-opacity-18 p-5 rounded-md cursor-pointer hover:scale-105 transition duration-300 mx-3 sm-m:min-w-[400px] sm:w-[300px] ">
                                 
                         <div>
-                            <p className=" text-md text-orange-700 font-semibold ">{actividad.edad.toUpperCase()}</p>
+                            {/* <p className=" text-md text-orange-700 font-semibold ">{actividad.edad.toUpperCase()}</p> */}
                             <h3 className=" text-orange-800 font-semibold text-2xl">{actividad.titulo}</h3>
                         </div>
                         <div className=" flex flex-col gap-2">
                             <p className="text-md text-orange-700">Área de desarrollo</p>
                             <div className=" flex flex-wrap gap-2">
                                 {actividad.area.map((area) => (
-                                <p key={area} className="p-2 text-md text-gray-100 bg-orange-700 shadow-md w-fit rounded-sm">{area}</p>
+                                <p key={area} className="p-2 text-md text-gray-100 bg-orange-700 shadow-md w-fit rounded-sm sm:text-xs">{area}</p>
                                 ))}
                             </div>
                         </div>
--- src/app/unAnio/page.jsx
+++ src/app/unAnio/page.jsx
@@ -14,7 +14,7 @@ export default function UnAnioPage (){
         <div className={darkMode ? "dark" : ""}>
             <section id={style.main} className="flex flex-col items-center justify-start min-h-screen home-dark bg-fixed bg-gradient-to-t from-violet-200 to-violet-400 dark:bg-gradient-to-t dark:from-slate-900 dark:to-indigo-950 gap-10 py-10 sm:py-10 sm:gap-5">
 
-                <div className="bg-white bg-opacity-70 shadow-lg backdrop-filter backdrop-blur-md border border-opacity-18 p-5 rounded-md text-center sm:mx-5 sm:p-3 sm:w-[300px] sm-m:mx-5 sm-m:p-3 sm-m:min-w-[400px]">
+                <div className="bg-white bg-opacity-70 shadow-lg backdrop-filter backdrop-blur-md border border-opacity-18 p-5 rounded-md text-center sm:mx-5 sm:p-3 sm:w-[300px] sm-m:mx-5 sm-m:p-3 sm-m:min-w-[400px] laptop:mx-5">
                     <p className=" text-orange-700 font-semibold text-lg sm:text-sm sm-m:text-base">"Los niños son el recurso más valioso y la esperanza más preciada para el futuro."</p>
                     <p className=" text-orange-600 font-light text-lg sm:text-sm sm-m:text-base">Lyndon B. Johnson</p>
                 </div>
