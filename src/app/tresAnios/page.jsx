04434221f8a6b1f343384288f5d6361584596cbb
santireca <santiagorecabarren7@gmail.com>
2023-07-02 22:40:04 -0300

    15th commit

--- src/app/tresAnios/page.jsx
+++ src/app/tresAnios/page.jsx
@@ -10,7 +10,7 @@ export default function tresAniosPage (){
 
     return (
         <div>
-            <section id={style.main} className="flex flex-col items-center justify-start min-h-screen home-dark bg-fixed bg-gradient-to-t from-violet-200 to-violet-400 dark:bg-gradient-to-t dark:from-slate-900 dark:to-indigo-950 gap-10 py-10 sm:py-10 sm:gap-5">
+            <section id={style.main} className="flex flex-col items-center justify-start min-h-screen overflow-hidden home-dark bg-fixed bg-gradient-to-t from-violet-200 to-violet-400 dark:bg-gradient-to-t dark:from-slate-900 dark:to-indigo-950 gap-10 py-10 sm:py-10 sm:gap-5">
 
                 <div className="bg-white bg-opacity-70 shadow-lg backdrop-filter backdrop-blur-md border border-opacity-18 p-5 rounded-md text-center sm:mx-5 sm:p-3 sm:w-[300px] sm-m:mx-5 sm-m:p-3 sm-m:min-w-[400px]">
                     <p className=" text-sky-700 font-semibold text-lg sm:text-sm sm-m:text-base">"Un niño no es un vaso que llenar, sino un fuego que encender."</p>
